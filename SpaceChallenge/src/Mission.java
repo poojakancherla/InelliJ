423afbc8c00d15feda8ad5ce24fb5b711d9b8da7
poojakancherla <pknq6@mst.edu>
2017-12-20 14:32:13 -0600

    Desiging mission specs

--- SpaceChallenge/src/Mission.java
+++ SpaceChallenge/src/Mission.java
@@ -33,7 +33,7 @@ public class Mission
                 return false;
         }
 
-        public static boolean launch(Item weight)
+        public static boolean carry(Item weight)
         {
             Item curr_weight = weight;
         }
@@ -41,17 +41,74 @@ public class Mission
 
     class Rocket implements Spaceship
     {
-        
+        public static boolean launch()
+        {
+            return true;
+        }
+
+        public static boolean land()
+        {
+            return true;
+        }
+
+        public static boolean canCarry(Item weight)
+        {
+            if (weight <= capacity)
+                return true;
+            else
+                return false;
+        }
+
+        public static boolean carry(Item weight)
+        {
+            Item curr_weight = weight;
+        }
     }
+
+
 
     class U1 extends Rocket
     {
+        launchExplosion = (int) 0.05 * (cargoCarried / cargoLimit);
+        landingCrash = (int) 0.01 * (cargoCarried / cargoLimit);
 
+        public static boolean launch()
+        {
+            if(launchExplosion >= 100)
+            return false;
+            else
+            return  true;
+        }
+
+        public static boolean land()
+        {
+            if(landingCrash >= 100)
+                return false;
+            else
+                return  true;
+        }
     }
 
     class U2 extends Rocket
     {
+        launchExplosion = (int) 0.04 * (cargoCarried / cargoLimit);
+        landingCrash = (int) 0.08 * (cargoCarried / cargoLimit);
 
+        public static boolean launch()
+        {
+            if(launchExplosion >= 100)
+                return false;
+            else
+                return  true;
+        }
+
+        public static boolean land()
+        {
+            if(landingCrash >= 100)
+                return false;
+            else
+                return  true;
+        }
     }
 
 
